4dbb53550debbbce7f50c37c1605b987b93704af
popo2381 <popo2381@icloud.com>
2026-02-10 10:53:38 +0900

    feat: PaymentRepository 추가
    - 결제완료 검증을 위한 멱등키 'portonePaymentId' db 조회 추가
    - 'orderId'를 사용하는 주문 조회 추가

--- src/main/java/com/spartaifive/commercepayment/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/spartaifive/commercepayment/domain/payment/repository/PaymentRepository.java
@@ -1,4 +1,12 @@
 package com.spartaifive.commercepayment.domain.payment.repository;
 
-public enum PaymentRepository {
+import com.spartaifive.commercepayment.domain.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByPortonePaymentId(String portonePaymentId);
+    List<Payment> findAllByOrder_id(Long orderId);
 }
